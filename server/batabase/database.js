6863134b866337cb00344ecf27cacf5551e9fd8a
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-07-29 04:57:15 +0000

    mongodb connection

---
+++ server/batabase/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const connectDB = mongoose.connect("mongodb://atlas-sql-652f6f1ef30f77480719340c-cojzm.a.query.mongodb.net/login?ssl=true&authSource=admin",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log("connexted");
+}).catch((err)=>{
+    console.log(err);
+});
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+require('./batabase/database');
 PORT = 3000;
+
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("app is running");
